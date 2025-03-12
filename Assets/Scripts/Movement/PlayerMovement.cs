bfa5f06a44dae81e4d2b79c4acd6d387584a8004
Drafj <juanpabloc2000@gmail.com>
2025-03-12 14:42:28 -0500

    new character sprite

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private PlayerInput playerInput;
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] SpriteRenderer sprite;
     [SerializeField] private float velocity;
 
     private void Move()
@@ -14,6 +15,11 @@ public class PlayerMovement : MonoBehaviour
         Vector2 movementInput = playerInput.actions["Move"].ReadValue<Vector2>();
 
         Vector3 actualDirection = new Vector3(movementInput.x, movementInput.y, 0);
+
+        if (actualDirection.x > 0)
+            sprite.flipX = false;
+        else if (actualDirection.x < 0)
+            sprite.flipX = true;
 
         rb.MovePosition(transform.position + actualDirection * velocity * Time.fixedDeltaTime);
     }
